ab7f25543775f9f903aa45be06e3e4da58c757e3
luaroncrew <ma-kir.ru@yandex.ru>
2022-06-07 16:23:58 +0200

    experience component

--- kirillgram/src/app/experiences/experiences.component.ts
+++ kirillgram/src/app/experiences/experiences.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Experience {
+  description: string;
+  date: string;
+  imageLink: string;
+}
+
+const EXPERIENCES: Experience []  = [
+  {
+    description: 'fasdkhfjgasdkjhgfasd',
+    date: 'when',
+    imageLink: '1231231231'
+  },
+  {
+    description: 'Ubikap',
+    date: 'when',
+    imageLink: '1231231231'
+  },
+  {
+    description: 'Yandex',
+    date: 'when',
+    imageLink: '1231231231'
+  },
+  {
+    description: 'IUT',
+    date: 'when',
+    imageLink: '1231231231'
+  },
+  {
+    description: 'far away',
+    date: 'when',
+    imageLink: '1231231231'
+  },
+  {
+    description: 'far away',
+    date: 'when',
+    imageLink: '1231231231'
+  },
+  {
+    description: 'far away',
+    date: 'when',
+    imageLink: '1231231231'
+  },
+  {
+    description: 'far away',
+    date: 'when',
+    imageLink: '1231231231'
+  },
+];
+
 @Component({
   selector: 'app-experiences',
   templateUrl: './experiences.component.html',
   styleUrls: ['./experiences.component.scss']
 })
+
 export class ExperiencesComponent implements OnInit {
-
-  constructor() { }
-
+  experiences = EXPERIENCES;
   ngOnInit(): void {
+    this.experiences = EXPERIENCES
   }
-
 }
